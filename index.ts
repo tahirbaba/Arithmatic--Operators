ed04e2d4feb45dc5c0b6b3a7a2a2fbf58c5971a8
tahirbaba <161713622+tahirbaba@users.noreply.github.com>
2024-03-31 21:13:52 +0500

    Practice of Arithmatic Operators with Typescript slide part 2

---
+++ index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+// Arithmatic Operators
+// (1) Addition
+console.log(2 + 3);  // in print : 5
+// with Varibales:
+let num1 = 5;
+let num2 = 5;
+let sum1 = (num1+num2);
+console.log(sum1);  // in print : 10
+
+// (2) Subtraction 
+console.log(3 - 2);  // in print : 1
+let sum2 = (num1-num2);
+console.log(sum2);  // in print : 0
+
+// (3) Multiplication 
+console.log(3*2);  // in print : 6
+let sum3 = (num1*num2);
+console.log(sum3);  // in print : 25
+
+// (4) Division 
+console.log(6/2);  // in print : 3
+let sum4 = (num1/num2);
+console.log(sum4);  // in print : 1
+
+// common
+console.log(30 - 20 + 10 * 40 / 2);  // in print : 210
+
+// module (%) = "it provides remainder of two numbers."
+let remainder = num1%num2 
+console.log(remainder);  // in print : 0
+
